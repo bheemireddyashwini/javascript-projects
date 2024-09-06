38eba274c246ac4f8f75fc3b3abf0b08b335364b
Ashwini Reddy <143033212+bheemireddyashwini@users.noreply.github.com>
2024-09-06 10:55:22 +0200

    Add files via upload
    Added the another file.

---
+++ solution.js
@@ -0,0 +1,22 @@
+/* let myNum = 15; 
+
+if (myNum % 3 === 0 && myNum % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (myNum % 3 === 0) {
+    console.log("Fizz");
+} else if (myNum % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(myNum);
+}
+ */
+
+  //Ternary Operator 
+
+ /* let myNum = 15;
+ solution = (myNum % 3 === 0 && myNum % 5 ===0) ? "FizzBuzz" : 
+ (myNum % 3 === 0) ? "Fizz" : 
+ (myNum % 5 === 0) ?  "Buzz" : myNum;
+
+console.log(solution);
+  */
